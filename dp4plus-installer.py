a4cd11f3694a9fbdd3c05b5c53ab6718d948ae37
RosarioCCLab <118339488+RosarioCCLab@users.noreply.github.com>
2023-03-21 14:49:17 -0300

    Create dp4plus-installer.py

---
+++ dp4plus-installer.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 21 14:45:03 2023
+
+@author: Franco, Bruno Agustín 
+
+This is a helper script to install a PyPI module, designed for users who are not familiar with operating system command line usage.
+"""
+
+import subprocess
+
+subprocess.run('pip install dp4plus-app') 
+
