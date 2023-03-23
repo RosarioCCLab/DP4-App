1111e7fa02ce8a07ed789f5c47b6cb9b12d37e32
RosarioCCLab <118339488+RosarioCCLab@users.noreply.github.com>
2023-03-23 14:50:59 -0300

    Update dp4plus-installer.py

--- dp4plus-installer.py
+++ dp4plus-installer.py
@@ -7,9 +7,8 @@ Created on Tue Mar 21 14:45:03 2023
 This is a helper script to install a PyPI module, designed for users who are not familiar with operating system command line usage.
 """
 
-import subprocess
+import subprocess, os, shutil
 
-subprocess.run('pip install dp4plus-app') 
 
 try: 
   subprocess.run('sudo apt-get update')
@@ -17,3 +16,15 @@ try:
 
 except: pass
 
+subprocess.run('pip install dp4plus-app') 
+
+def create_exe():
+    '''Creates a direc acces executable file in the user desktop'''
+    #desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+    desktop = os.path.normpath(os.path.expanduser("~/Desktop"))
+    exe = shutil.which("dp4plus")
+    
+    shutil.copy(exe, desktop)
+    return 
+  
+  create_exe()
