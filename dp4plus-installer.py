7f019ccba0b6fb271867dcb356fa59e78fb8ecd3
RosarioCCLab <118339488+RosarioCCLab@users.noreply.github.com>
2023-06-13 11:59:36 -0300

    Update dp4plus-installer.py

--- dp4plus-installer.py
+++ dp4plus-installer.py
@@ -15,9 +15,13 @@ import subprocess, os, shutil
 
 if ('Windows' in operating_system or 
    'Darwin' in operating_system): 
-  subprocess.run('pip install --upgrade pip')
-  subprocess.run('pip install --upgrade dp4plus-app') 
-                 
+   try:
+      subprocess.run('pip install --upgrade pip')
+      subprocess.run('pip install --upgrade dp4plus-app')
+   else:
+      subprocess.run('pip3 install --upgrade pip')
+      subprocess.run('pip3 install --upgrade dp4plus-app')
+      
 elif 'Linux' in operating_system: 
   os.system('sudo apt-get install python3-pip')
   os.system('sudo apt-get install python3-tk')
